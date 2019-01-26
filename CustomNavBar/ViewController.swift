fa55d771491e15ce9cb499d7c1af61b1b1e91adc
Praveenkumar Somu <praveen.kumar@aleph-labs.com>
2019-01-26 16:26:51 +0700

    Initial Commit

---
+++ CustomNavBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomNavBar
+//
+//  Created by Praveenkumar Somu on 26/1/2562 BE.
+//  Copyright © 2562 Praveenkumar Somu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
